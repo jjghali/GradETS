0804122e7f346b3388e20c51796d0b1bd85a8ca9
Joshua Ghali <jjghali@gmail.com>
2016-06-27 17:14:00 -0400

    added actionbar

--- app/src/main/java/geniets/android/activities/MainActivity.java
+++ app/src/main/java/geniets/android/activities/MainActivity.java
@@ -4,16 +4,24 @@ import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import geniets.android.R;
 import geniets.android.fragments.TodayTaskFragment;
 
 public class MainActivity extends Activity {
 
+    @BindView(R.id.toolbar_title)
+    TextView toolbar_title;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        toolbar_title.setText("suprise! BOB!");
 
         FragmentManager fragmentManager = getFragmentManager();
 
@@ -21,8 +29,9 @@ public class MainActivity extends Activity {
 
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        fragmentTransaction.add(R.id.fragment_container, todayTaskFragment);
+        fragmentTransaction.add(R.id.main_fragmentContainer, todayTaskFragment);
         fragmentTransaction.commit();
+
 
     }
 }
