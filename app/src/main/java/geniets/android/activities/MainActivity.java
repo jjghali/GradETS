0a94a27b1c4aff7b2c9ad0bf4455a1cec8d8f68d
Joshua Ghali <jjghali@gmail.com>
2016-07-01 19:55:12 -0400

    fix navdrawer icons

--- app/src/main/java/geniets/android/activities/MainActivity.java
+++ app/src/main/java/geniets/android/activities/MainActivity.java
@@ -3,15 +3,13 @@ package geniets.android.activities;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+
 import android.widget.TextView;
 import android.widget.Toolbar;
-
-import org.w3c.dom.Text;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -48,12 +46,15 @@ public class MainActivity extends Activity {
         toolbar_title.setText("Home");
         navbarTitle.setText(etudiant.prenom.trim() + " " + etudiant.nom.trim());
         navbarSubtitle.setText(etudiant.codePerm.trim());
-
+        navigationView.setItemIconTintList(null);
 
         ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer);
 
         drawerLayout.setDrawerListener(drawerToggle);
         drawerToggle.syncState();
+
+
+
         FragmentManager fragmentManager = getFragmentManager();
 
         TodayTaskFragment todayTaskFragment = new TodayTaskFragment();
