4472e1733da2508d76c50107ac5959667a8bbb6e
Joshua Ghali <jjghali@gmail.com>
2016-09-30 22:19:26 -0400

    modified the menu and the today fragment

--- app/src/main/java/me/jjghali/grades/android/fragments/HomeFragment.java
+++ app/src/main/java/me/jjghali/grades/android/fragments/HomeFragment.java
@@ -1,14 +1,18 @@
 package me.jjghali.grades.android.fragments;
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import grades.android.R;
 
 
@@ -20,18 +24,30 @@ public class HomeFragment extends Fragment {
     TextView todayCardTitle;
 
     public HomeFragment() {
+
         // Required empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_today_task, container, false);
+        ButterKnife.bind(this, view);
+
         return inflater.inflate(R.layout.fragment_today_task, container, false);
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+
+    /**
+     * Discards all the tasks showing up
+     */
+    @OnClick(R.id.tasksDiscardAll)
+    public void discardAllTasks() {
+
     }
 }
